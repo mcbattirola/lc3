700014eaa7152b19c6080bc836d30fcead951d5e
matheus c. b <mcbattirola@hotmail.com>
2024-12-08 09:56:43 -0300

    Add op tests; minor refactors

--- src/lc3.zig
+++ src/lc3.zig
@@ -6,7 +6,7 @@ const MEMORY_SIZE = 1 << 16;
 const PC_START = 0x3000;
 
 // register indexes
-const reg = enum(usize) {
+pub const reg_idx = enum(usize) {
     r0 = 0,
     r1,
     r2,
@@ -18,10 +18,17 @@ const reg = enum(usize) {
     pc,
     cond,
 
-    pub fn val(self: reg) usize {
+    pub fn val(self: reg_idx) usize {
         return @intFromEnum(self);
     }
 };
+
+pub const Registers = [10]u16;
+pub fn newRegisters() Registers {
+    var r: Registers = undefined;
+    @memset(&r, 0);
+    return r;
+}
 
 // condition flags
 const flag = enum(u8) {
@@ -32,7 +39,7 @@ const flag = enum(u8) {
 
 pub const LC3 = struct {
     memory: [MEMORY_SIZE]u16 = undefined,
-    registers: [10]u16 = undefined,
+    registers: Registers = newRegisters(),
 
     running: bool = true,
 
@@ -41,7 +48,7 @@ pub const LC3 = struct {
         self.memory[qty - 1] = 10;
         std.debug.print("First {d} bytes: {any}!\n", .{ qty, self.memory[0..qty] });
 
-        self.registers[reg.pc.val()] = PC_START;
+        self.registers[reg_idx.pc.val()] = PC_START;
         while (self.running) {
             const instruction = self.fetch();
             const op: OP = @enumFromInt(instruction >> 12);
@@ -50,7 +57,7 @@ pub const LC3 = struct {
                 // OP.BR => {},
                 OP.ADD => self.add(instruction),
                 else => {
-                    self.printRegisters();
+                    printRegisters(self.registers);
                     self.running = false;
                 },
             }
@@ -59,10 +66,10 @@ pub const LC3 = struct {
 
     // returns an instruction from memory and increments pc
     pub fn fetch(self: *LC3) u16 {
-        if (self.registers[reg.pc.val()] == PC_START) {
+        if (self.registers[reg_idx.pc.val()] == PC_START) {
             std.debug.print("return ADD\n", .{});
             self.incrementPC();
-            return 0b0001_111_001_1_00010;
+            return 0b0001_111_001_1_00011;
         }
         // TODO: fetch from memory
 
@@ -71,7 +78,7 @@ pub const LC3 = struct {
     }
 
     pub fn incrementPC(self: *LC3) void {
-        self.registers[reg.pc.val()] += 1;
+        self.registers[reg_idx.pc.val()] += 1;
     }
 
     pub fn add(self: *LC3, instruction: u16) void {
@@ -88,20 +95,26 @@ pub const LC3 = struct {
         const sr2 = instruction & 0b111;
         self.registers[dr] = self.registers[sr1] + self.registers[sr2];
     }
-
-    // prints the state of the the machine
-    pub fn printRegisters(self: *LC3) void {
-        std.debug.print("R0: {d}\nR1: {d}\nR2: {d}\nR3: {d}\nR4: {d}\nR5: {d}\nR6: {d}\nR7: {d}\nPC: {d}\nCOND: {d}\n", .{
-            self.registers[reg.r0.val()],
-            self.registers[reg.r1.val()],
-            self.registers[reg.r2.val()],
-            self.registers[reg.r3.val()],
-            self.registers[reg.r4.val()],
-            self.registers[reg.r5.val()],
-            self.registers[reg.r6.val()],
-            self.registers[reg.r7.val()],
-            self.registers[reg.pc.val()],
-            self.registers[reg.cond.val()],
-        });
+    // returns the registers, used in test
+    pub fn getRegisters(self: *LC3) Registers {
+        return self.registers;
+    }
+    pub fn setRegisters(self: *LC3, r: Registers) void {
+        self.registers = r;
     }
 };
+
+pub fn printRegisters(registers: Registers) void {
+    std.debug.print("R0: {d}\nR1: {d}\nR2: {d}\nR3: {d}\nR4: {d}\nR5: {d}\nR6: {d}\nR7: {d}\nPC: {d}\nCOND: {d}\n", .{
+        registers[reg_idx.r0.val()],
+        registers[reg_idx.r1.val()],
+        registers[reg_idx.r2.val()],
+        registers[reg_idx.r3.val()],
+        registers[reg_idx.r4.val()],
+        registers[reg_idx.r5.val()],
+        registers[reg_idx.r6.val()],
+        registers[reg_idx.r7.val()],
+        registers[reg_idx.pc.val()],
+        registers[reg_idx.cond.val()],
+    });
+}
--- src/lc3_test.zig
+++ src/lc3_test.zig
@@ -1,6 +1,54 @@
 const std = @import("std");
 const t = std.testing;
 
-test "sometest" {
-    try t.expect(1 == 1);
+const lc3 = @import("lc3.zig");
+const LC3 = lc3.LC3;
+const Registers = lc3.Registers;
+const reg_idx = lc3.reg_idx;
+
+test "add op" {
+    // immediate mode
+    {
+        // dr = r7, sr=1, imm5 = 3
+        var instruction: u16 = 0b0001_111_001_1_00010;
+        var vm = LC3{};
+        vm.add(instruction);
+
+        var expected: Registers = lc3.newRegisters();
+        expected[reg_idx.r7.val()] = 2;
+
+        try expectEqualRegisters(expected, vm.getRegisters());
+
+        // update r1 value and check again
+        var vmr = vm.getRegisters();
+        vmr[reg_idx.r1.val()] = 10;
+        vm.setRegisters(vmr);
+        vm.add(instruction);
+
+        expected[reg_idx.r1.val()] = 10;
+        expected[reg_idx.r7.val()] = 12;
+        try expectEqualRegisters(expected, vm.getRegisters());
+
+        // dr = r0, sr=r2, imm5 = 0
+        instruction = 0b0001_000_010_1_00000;
+        vm = LC3{};
+        vm.add(instruction);
+        expected = lc3.newRegisters();
+        expected[reg_idx.r0.val()] = 0;
+        // update r2 and check again
+        vmr = vm.getRegisters();
+        vmr[reg_idx.r2.val()] = 10_000;
+        vm.setRegisters(vmr);
+        vm.add(instruction);
+
+        expected[reg_idx.r2.val()] = 10_000;
+        expected[reg_idx.r0.val()] = 10_000;
+        try expectEqualRegisters(expected, vm.getRegisters());
+    }
+}
+
+fn expectEqualRegisters(expected: Registers, actual: Registers) !void {
+    for (expected, 0..) |_, i| {
+        try t.expectEqual(expected[i], actual[i]);
+    }
 }
