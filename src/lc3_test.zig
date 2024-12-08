7ff841e6aa8ad2041faaf0bdfcced7a0377c8a93
matheus c. b <mcbattirola@hotmail.com>
2024-12-08 17:37:12 -0300

    Rename methods

--- src/lc3_test.zig
+++ src/lc3_test.zig
@@ -16,20 +16,20 @@ test "add" {
 
     instruction = add(reg_idx.r1, reg_idx.r1, reg_idx.r1);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r7.val()] = 0;
     expected[reg_idx.cond.val()] = flag.zero.val();
     try expectEqualRegisters(expected, vm.registers);
     vm.registers[reg_idx.r1.val()] = 10;
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected[reg_idx.r1.val()] = 20;
     expected[reg_idx.cond.val()] = flag.pos.val();
     try expectEqualRegisters(expected, vm.registers);
 
     instruction = add(reg_idx.r1, reg_idx.r2, reg_idx.r3);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r7.val()] = 0;
     expected[reg_idx.cond.val()] = flag.zero.val();
@@ -37,7 +37,7 @@ test "add" {
     vm.registers[reg_idx.r1.val()] = 10;
     vm.registers[reg_idx.r2.val()] = 20;
     vm.registers[reg_idx.r3.val()] = 30;
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected[reg_idx.r1.val()] = 50;
     expected[reg_idx.r2.val()] = 20;
     expected[reg_idx.r3.val()] = 30;
@@ -54,7 +54,7 @@ test "add immediate" {
     // dr = r7, sr = 1, imm5 = 3
     instruction = addI(reg_idx.r7, reg_idx.r1, 3);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r7.val()] = 3;
     expected[reg_idx.cond.val()] = flag.pos.val();
@@ -62,7 +62,7 @@ test "add immediate" {
 
     // update r1 value and check again
     vm.registers[reg_idx.r1.val()] = 10;
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected[reg_idx.r1.val()] = 10;
     expected[reg_idx.r7.val()] = 13;
     try expectEqualRegisters(expected, vm.registers);
@@ -70,12 +70,12 @@ test "add immediate" {
     // dr = r0, sr = r2, imm5 = 0
     instruction = addI(reg_idx.r0, reg_idx.r2, 0);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r0.val()] = 0;
     // update r2 and check again
     vm.registers[reg_idx.r2.val()] = 10_000;
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected[reg_idx.r2.val()] = 10_000;
     expected[reg_idx.r0.val()] = 10_000;
     expected[reg_idx.cond.val()] = flag.pos.val();
@@ -84,18 +84,18 @@ test "add immediate" {
     // dr = r1, sr = r1, imm5 = 5
     instruction = addI(reg_idx.r1, reg_idx.r1, 5);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r1.val()] = 65;
     vm.registers[reg_idx.r1.val()] = 60;
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected[reg_idx.cond.val()] = flag.pos.val();
     try expectEqualRegisters(expected, vm.registers);
 
     // dr = r1, sr = r1, imm5 = -1
     instruction = addI(reg_idx.r1, reg_idx.r1, 65535);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r1.val()] = 65535; // -1 in two's complement
     expected[reg_idx.cond.val()] = flag.neg.val();
@@ -104,20 +104,20 @@ test "add immediate" {
     vm.registers[reg_idx.r1.val()] = 1;
     expected[reg_idx.r1.val()] = 0;
     expected[reg_idx.cond.val()] = flag.zero.val();
-    vm.add(instruction);
+    vm.opAdd(instruction);
     try expectEqualRegisters(expected, vm.registers);
 
     // dr = r1, sr = r1, imm5 = -4
     instruction = addI(reg_idx.r1, reg_idx.r1, 65532);
     vm = LC3{};
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r1.val()] = 65532; // -4 in two's complement
     expected[reg_idx.cond.val()] = flag.neg.val();
 
     try expectEqualRegisters(expected, vm.registers);
     vm.registers[reg_idx.r1.val()] = 128;
-    vm.add(instruction);
+    vm.opAdd(instruction);
     expected[reg_idx.r1.val()] = 124;
     expected[reg_idx.cond.val()] = flag.pos.val();
     try expectEqualRegisters(expected, vm.registers);
@@ -130,13 +130,13 @@ test "and" {
 
     instruction = andNonImm(reg_idx.r1, reg_idx.r1, reg_idx.r1);
     vm = LC3{};
-    vm.andOp(instruction);
+    vm.opAnd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r1.val()] = 0;
     expected[reg_idx.cond.val()] = flag.zero.val();
     try expectEqualRegisters(expected, vm.registers);
     vm.registers[reg_idx.r1.val()] = 20;
-    vm.andOp(instruction);
+    vm.opAnd(instruction);
     expected[reg_idx.r1.val()] = 20;
     expected[reg_idx.cond.val()] = flag.pos.val();
     try expectEqualRegisters(expected, vm.registers);
@@ -145,7 +145,7 @@ test "and" {
     vm = LC3{};
     vm.registers[reg_idx.r3.val()] = 0b00001111;
     vm.registers[reg_idx.r4.val()] = 0b11111111;
-    vm.andOp(instruction);
+    vm.opAnd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r2.val()] = 0b00001111;
     expected[reg_idx.r3.val()] = 0b00001111;
@@ -161,7 +161,7 @@ test "and immediate" {
 
     instruction = andI(reg_idx.r1, reg_idx.r1, 65535);
     vm = LC3{};
-    vm.andOp(instruction);
+    vm.opAnd(instruction);
     expected = lc3.newRegisters();
     expected[reg_idx.r1.val()] = 0;
     expected[reg_idx.cond.val()] = flag.zero.val();
@@ -170,7 +170,7 @@ test "and immediate" {
     vm.registers[reg_idx.r1.val()] = 65535;
     expected[reg_idx.r1.val()] = 65535;
     expected[reg_idx.cond.val()] = flag.neg.val();
-    vm.andOp(instruction);
+    vm.opAnd(instruction);
     try expectEqualRegisters(expected, vm.registers);
 
     instruction = andI(reg_idx.r2, reg_idx.r3, 5);
@@ -180,7 +180,7 @@ test "and immediate" {
     expected[reg_idx.r2.val()] = 1;
     expected[reg_idx.r3.val()] = 1;
     expected[reg_idx.cond.val()] = flag.pos.val();
-    vm.andOp(instruction);
+    vm.opAnd(instruction);
     try expectEqualRegisters(expected, vm.registers);
 }
 
@@ -206,6 +206,7 @@ fn andNonImm(comptime dr: reg_idx, comptime sr1: reg_idx, comptime sr2: reg_idx)
     return op(0b0101, dr, sr1, sr2);
 }
 
+// works for ADD and AND
 fn immOp(comptime code: u16, comptime dr: reg_idx, comptime sr: reg_idx, comptime imm5: u16) u16 {
     const opcode = code << 12;
     const dr_mask = @intFromEnum(dr) << 9; // Destination register
@@ -215,6 +216,7 @@ fn immOp(comptime code: u16, comptime dr: reg_idx, comptime sr: reg_idx, comptim
     return opcode | dr_mask | sr_mask | mode | imm5_mask;
 }
 
+// works for ADD and AND
 fn op(comptime code: u16, comptime dr: reg_idx, comptime sr1: reg_idx, comptime sr2: reg_idx) u16 {
     const opcode = code << 12;
     const dr_mask = @intFromEnum(dr) << 9; // Destination register
