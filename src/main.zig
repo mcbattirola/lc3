c4d2a31ef5d1ea48f96543e3638b39b9b8964ac7
matheus c. b <mcbattirola@hotmail.com>
2024-12-14 09:12:30 -0300

    Add raylib

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,13 @@
+const std = @import("std");
 const LC3 = @import("lc3.zig").LC3;
+const rl = @import("raylib");
 
 pub fn main() !void {
     // TODO: CLI
+
+    // raylib call example
+    std.debug.print("mouse at {?}\n", .{rl.getMousePosition()});
+
     var vm = LC3{};
     vm.run();
 }
